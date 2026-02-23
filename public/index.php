c347dd7ed8032e8c0c44cd697c3d953ffada4a4e
aleksey bern <albern79@gmail.com>
2026-02-23 11:08:52 +0700

    change index.php add dependenses

--- public/index.php
+++ public/index.php
@@ -4,14 +4,53 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+//use Illuminate\Support\Collection;
 
-$app = AppFactory::create();
+use function Symfony\Component\String\s;
+
+$container = new Container();
+
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
+      $response2 = $response->withStatus(204);
     return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
 });
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $result = collect($users)->filter(
+        fn($user) => empty($term) ? true : s($user)->ignoreCase()->startsWith($term)
+    );
+    $params = [
+        'users' => $result,
+        'term' => $term
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, '/users/show.phtml', $params);
+});
+
+
+
+$app->get('/companies', function ($request, $response) {
+    return $response->write('GET /companies');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
 $app->run();
